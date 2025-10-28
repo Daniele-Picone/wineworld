c6e75a102e4074f00a378ce81b31bb2a48709bde
Daniele Picone <danielepicone97@gmail.com>
2025-10-28 11:44:14 +0100

    favicon fix 2

--- app/layout.js
+++ app/layout.js
@@ -21,9 +21,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-  <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+     <head>
+ <link rel="icon" href="/favicon.ico" type="image/x-icon" />
   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+
 </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <UserProvider>
