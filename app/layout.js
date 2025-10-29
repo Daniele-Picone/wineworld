f3d51874a6c19587accd5c838a9b13d84161c959
Daniele Picone <danielepicone97@gmail.com>
2025-10-29 22:44:13 +0100

    Aggiunta icone in meta

--- app/layout.js
+++ app/layout.js
@@ -22,13 +22,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
      <head>
- <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+<link rel="icon" href="/favicon.ico" type="image/x-icon" />
   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  {/* <!-- PNG Icons per vari dispositivi --> */}
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+  <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
 
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+  {/* <!-- Icona per dispositivi Apple --> */}
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+  {/* <!-- Web App Manifest --> */}
+  <link rel="manifest" href="/site.webmanifest" />
 
 </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
