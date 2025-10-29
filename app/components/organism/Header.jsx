66a6e69e460c42b93a9c8ebb653c306d4d95e942
Daniele Picone <danielepicone97@gmail.com>
2025-10-29 14:59:16 +0100

    modifica Logo/link e media query

--- app/components/organism/Header.jsx
+++ app/components/organism/Header.jsx
@@ -2,11 +2,12 @@
 
 import './Header.css';
 import NavLinks from '@/app/components/molecules/NavLinks';
+import Link from 'next/link';
 
 export default function  Header(){
     return(
         <header className="header" >
-            <div className="logo"><h1>WineWorld</h1> </div>
+            <div className="logo"><Link href='/' ><h1>WineWorld</h1></Link> </div>
             <NavLinks/>
         </header>
     )
