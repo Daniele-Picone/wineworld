d228f51e89c4926510e67df157779a4977d2ffc7
Daniele Picone <danielepicone97@gmail.com>
2025-11-01 11:31:48 +0100

    aggiunto sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://www.wineworldweb.it',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  exclude: ['/admin/*', '/dashboard/','/dashboard/*','/api/*'],
+};
+
+export default config;
