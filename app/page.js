bffb03277977b3e68620ff4fd9fdcbac6829047f
Daniele Picone <danielepicone97@gmail.com>
2026-06-30 10:41:41 +0200

    rimosso wewino

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
 import Image from "next/image";
 import MainLayout from "@/app/components/layouts/MainLayout";
 import HeroSection from "@/app/components/organism/HeroSection"
-import WineSection from "./components/organism/WineSection";  
 import CardsSection from "./components/organism/CardsSection";
-import WewinoSection from "./components/organism/WewinoSection";
+
 
 export const metadata = {
   title: "WineWorld | Esplora il mondo del vino",
@@ -19,7 +18,7 @@ export default function Home() {
         <HeroSection />
         <CardsSection />
         {/* <WineSection /> */}
-        <WewinoSection />
+
       </MainLayout>
     </div>
   );
